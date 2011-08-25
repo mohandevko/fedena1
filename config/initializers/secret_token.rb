72fb8b159bbae6008275a15d5b7c564757efe469
manu <manu@manu-desktop.(none)>
2011-08-25 09:04:10 +0530

    Implemented authentication using devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechCredentials::Application.config.secret_token = '0129286b4fcc4d5016a0d7e94fa71dacaf52240f5856bf81ded122fe868c4b7c9e41fb93900480d93963470e182c108c5cefae5ffabef8b13e57e2f7636a95df'
