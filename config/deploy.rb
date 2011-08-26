a0a3fc3d3a7c24a8034845a418fd4fad4fce5ccb
Harish P <harishp@8raystech.com>
2011-08-27 00:06:12 +0530

    Modified .gitignore, Implemented capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
+
+set :application, "tech-credentials"
+set :repository,  "git@dine-media.com:tech-credentials.git"
+set :scm, :git
+set :scm_user, "rails"
+
+desc "Symlinks database.yml, mailer.yml file from shared directory into the latest release"
+task :symlink_shared, :roles => [:app, :db] do
+  run "ln -s #{shared_path}/database.yml #{latest_release}/config/database.yml"
+end
+
+after 'deploy:finalize_update', :symlink_shared
+
+namespace :deploy do
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
