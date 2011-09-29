8aba75c45fb6b46c76485ab78871056d760b4663
Mohankumar K.O <mohandev.ko@gmail.com>
2011-09-29 21:24:41 +0530

    Search feature modified

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,9 @@ class SearchController < ApplicationController
   end
   
   def result
-    @users = User.find_all_by_first_name(params[:s])
-    @skills = Skill.find_all_by_name(params[:s])
-    @educations = Education.find_all_by_qualification(params[:s])
+    @users = User.where("first_name like ? OR last_name like ? OR email like ?", params[:s] + "%", params[:s] + "%", "%" + params[:s] + "%")
+    @skills = Skill.where("name like ? ", params[:s] + "%")
+    @educations = Education.where("qualification like ? ", params[:s] + "%")
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
   def dashboard
     @menu = "dashboard"
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
     @skills = current_user.skills
     @work_histories = current_user.work_histories
     @wk = current_user.work_histories
