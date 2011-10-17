0d95d08829f3c6210eca6e35d88ea78392700add
Mohankumar K.O <mohandev.ko@gmail.com>
2011-10-17 19:05:05 +0530

    deleted add_email_column_to_messages

--- db/migrate/20111016061605_add_email_column_to_message_table.rb
+++
@@ -1,9 +0,0 @@
-class AddEmailColumnToMessageTable < ActiveRecord::Migration
-  def self.up
-    add_column :messages, :email, :string
-  end
-
-  def self.down
-    :remove_column :messages, :email
-  end
-end
